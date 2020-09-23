128bbb34f138b0c6f6c051940ded2acd351c84dc
the-duckchild <37538808+the-duckchild@users.noreply.github.com>
2020-09-23 23:14:50 +0100

    Add files via upload

---
+++ Robodesk_3.0.ino
@@ -0,0 +1,14 @@
+#include <MIDI_Controller.h>
+      //Midi controller library
+
+AnalogMultiplex multiplexer( A0, { 10, 11, 12 } );
+
+void setup() {
+  // put your setup code here, to run once:
+
+}
+
+void loop() {
+  // put your main code here, to run repeatedly:
+
+}
